8dc501c52675bc13c013750914b8afcdc8d22de6
Ekagamjot Singh <ekagam05@gmail.com>
2025-01-26 15:36:54 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -10,4 +10,15 @@ int main() {
    {
     printf("B");
    }
+   else if(70<=i<80)
+   {
+    printf("C");
+   }
+   else if(60<=i<70)
+   {
+    printf("D");
+   }
+   else{
+    printf("F");
+   }
 }
