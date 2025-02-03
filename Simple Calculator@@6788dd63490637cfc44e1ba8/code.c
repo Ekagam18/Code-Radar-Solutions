fddff49a76729b3cad347ff1637174461fe6438d
Ekagamjot Singh <ekagam05@gmail.com>
2025-02-03 13:53:55 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,10 +6,10 @@ int main()
     scanf("%d %d",&i,&j);
     scanf("%c",&op);
     float d,e,f,g;
-    d=i+j;
-    e=i-j;
-    f=i*j;
-    g=i/j;
+    d= i+j;
+    e= i-j;
+    f= i*j;
+    g= i/j;
  
     switch(op)
     {
