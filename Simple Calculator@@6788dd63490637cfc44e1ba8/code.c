8ab61f86fd4aff3b900aa3610b5ca9c6d562f262
Ekagamjot Singh <ekagam05@gmail.com>
2025-02-03 13:55:49 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@ int main()
 {
     float i,j;
     char op;
-    scanf("%d %d",&i,&j);
+    scanf("%f %f",&i,&j);
     scanf("%s",&op);
     float d,e,f,g;
     d= i+j;
@@ -16,7 +16,6 @@ int main()
         case'+':
           printf("%d",i+j);
           break;
-
         case'-':
            printf("%d",e);
            break;
